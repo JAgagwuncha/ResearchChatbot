0276febc8d11480430ba37d68d9d484a115a4b43
Joseph Agagwuncha <jocom1@hotmail.co.uk>
2022-04-13 17:47:58 +0100

    Added input box

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from "./components/Test"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Test/>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
---
+++ chatbot/src/components/Test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Test = () => {
+    return (
+        <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+
+    );
+};
+export default Test;
